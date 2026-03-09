3b353d453d56138b2e1d1ef976954c4d9391f019
Katranyan Vagram <vagram.katr@gmail.com>
2026-03-09 21:24:43 +0300

    Добавлены новые модели и обновления: - Добавлено поле owner в BusinessElement - Добавлено поле update_all_permission в AccessRoleRule - Созданы модели User и JWT - Применены миграции и создан суперпользователь

--- permissions/models.py
+++ permissions/models.py
@@ -1,4 +1,29 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    patronymic = models.CharField(max_length=50,blank=True,null=True)
+    email = models.EmailField(unique=True)
+    password_hash = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+
+    role = models.ForeignKey('Role',on_delete=models.SET_NULL, null=True)
+
+    def set_password(self, raw_password):
+        self.password_hash= make_password(raw_password)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
+
+class JWT(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    token = models.TextField()
+    expire_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"JWT for {self.user.email}"
 
 class Role(models.Model):
     name = models.CharField(max_length=50,unique=True)
@@ -8,6 +33,7 @@ class Role(models.Model):
 
 class BusinessElement(models.Model):
     name = models.CharField(max_length=100,unique=True)
+    owner = models.ForeignKey('User',on_delete=models.CASCADE,null=True,blank=True)
 
     def __str__(self):
         return self.name
@@ -20,6 +46,7 @@ class AccessRoleRule(models.Model):
     read_all_permission = models.BooleanField(default=False)
     create_permission = models.BooleanField(default=False)
     update_permission = models.BooleanField(default=False)
+    update_all_permission = models.BooleanField(default=False)
     delete_permission = models.BooleanField(default=False)
     delete_all_permission = models.BooleanField(default=False)
 
