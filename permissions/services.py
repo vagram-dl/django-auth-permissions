bc1e52896c4a7965b9645718660abcb8c6b59ce3
Katranyan Vagram <vagram.katr@gmail.com>
2026-08-01 09:20:32 +0300

    refactor: применение SRP (Single Responsibility) в AuthService
    - Вынесена логика генерации JWT в отдельный метод _create_jwt_token
    - Функция login_user теперь отвечает только за валидацию и аутентификацию

--- permissions/services.py
+++ permissions/services.py
@@ -20,11 +20,8 @@ class AuthService:
         if user is None or not user.is_active:
             raise ValueError("Invalid credentials")
 
-        payload = {"user_id" : user.id}
-        token = jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
-
-        expire_at = timezone.now() + timedelta(hours=1)
-        JWT.objects.create(user=user, token=token, expire_at=expire_at)
+        token,expire_at = AuthService._create_jwt_token(user)
+        JWT.objects.create(user=user, token=token,expire_at=expire_at)
 
         return {
             "token" : token,
@@ -33,6 +30,11 @@ class AuthService:
         }
 
     @staticmethod
+    def _create_jwt_token(user):
+            payload = {"user_id":user.id}
+            token = jwt.encode(payload,settings.SECRET_KEY,algorithm="HS256")
+            expire_at = timezone.now() + timedelta(hours=1)
+            return token, expire_at
     def logout_user(user):
         JWT.objects.filter(user=user).delete()
         return {"message" : "Logged out successfully"}
