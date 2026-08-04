71492c5e9660ae05678afd9fd0cd5e441e5b9b07
Katranyan Vagram <vagram.katr@gmail.com>
2026-08-04 17:11:56 +0300

    ci: настройка GitHub Actions и добавление зависимостей
    - Перемещен .github в корень проекта (требование GitHub Actions)
    - Добавлен requirements.txt для установки пакетов
    - Исправлен негативный тест test_login_user_invalid_password

--- permissions/tests/test_services.py
+++ permissions/tests/test_services.py
@@ -27,3 +27,15 @@ class AuthServiceTests(TestCase):
         self.assertIsNotNone(call_args[2])
         self.assertIn("token",result)
         self.assertEqual(result["email"],self.user.email)
+
+    @patch('permissions.services.TokenRepository.save')
+    def test_login_user_invalid_password(self, mock_save):
+        login_data = {
+            "email" : "mock@test.com",
+            "password" : "wrong_password_123"
+        }
+
+        with self.assertRaises(ValueError) as context:
+            AuthService.login_user(login_data)
+        self.assertIn("Invalid credentials",str(context.exception))
+        mock_save.assert_not_called()
