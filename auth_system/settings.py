f26805277cbed1e5def53b37ae4f9fde1724feb2
Katranyan Vagram <vagram.katr@gmail.com>
2026-08-07 09:35:28 +0300

    feat: добавлена базовая конфигурация логирования

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -167,3 +167,40 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGGING = {
+    'version' : 1,
+    'disable_existing_loggers' : False,
+    'formatters' : {
+        'verbose' : {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+
+        },
+    },
+    'handlers' : {
+        'console' : {
+            'level' : 'DEBUG',
+            'class' : 'logging.StreamHandler',
+            'formatter' : 'verbose'
+        },
+        'file' : {
+            'level' : 'INFO',
+            'class' : 'logging.FileHandler',
+            'filename': 'auth_system.log',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers' : {
+        'permissions' : {
+            'handlers' : ['console','file'],
+            'level' : 'DEBUG',
+            'propagate' : False
+        },
+        'users': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
